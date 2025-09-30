7020a53074b63994de212e11b6be5e24e478cbbc
codegod <ashutheola@gmail.com>
2025-09-30 22:22:33 +0100

    tryin to store name and mark

--- 0xstudent_grades.py
+++ 0xstudent_grades.py
@@ -7,20 +7,18 @@
 student_name = [30]
 student_mark = [3]
 student_name = input("Enter student's name: ")
-print("Hello,", student_name)  
+print("Name:", student_name)  
 student_mark = input("Enter student's mark: ")  
-print("Hello,", student_mark)   
+print("Score:", student_mark)   
 
 
-for key, value in student_mark.items():
-    print(f"{key}:{value}")
     
     
 def user_info(name, age):   
     print(f"{name} is {age} years old") 
 user_info(age=25, name="ALice")
 
-num = 10
+grade = 10
 if num >= 20:
     if num < 30:
         print ("NUmber is between 20 and 29")
