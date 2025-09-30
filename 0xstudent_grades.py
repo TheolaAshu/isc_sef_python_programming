01d755230c40811ae47435d711fdc0fddf795f05
codegod <ashutheola@gmail.com>
2025-09-30 22:08:21 +0100

    progress

--- 0xstudent_grades.py
+++ 0xstudent_grades.py
@@ -1,6 +1,30 @@
-user = {
-    "name": "Alice",
-    "mark": 25,
-}
-print(user["name"])          # Alice
-print(user["mark"])           # update value
+# user = {
+#     "name": "Alice",
+#     "mark": 25,
+# }
+# print(user["name"])          
+# print(user["mark"])     
+student_name = [30]
+student_mark = [3]
+student_name = input("Enter student's name: ")
+print("Hello,", student_name)  
+student_mark = input("Enter student's mark: ")  
+print("Hello,", student_mark)   
+
+
+for key, value in student_mark.items():
+    print(f"{key}:{value}")
+    
+    
+def user_info(name, age):   
+    print(f"{name} is {age} years old") 
+user_info(age=25, name="ALice")
+
+num = 10
+if num >= 20:
+    if num < 30:
+        print ("NUmber is between 20 and 29")
+    else:
+        print("number is 30 or greater")
+else:
+    print("Number is less than 20")
