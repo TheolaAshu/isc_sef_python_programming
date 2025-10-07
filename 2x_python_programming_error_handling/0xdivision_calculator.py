738a707d4faeb8476bd5bd40c5e361ef52b9177e
codegod <ashutheola@gmail.com>
2025-10-07 18:50:13 +0100

    implementation of ZeroDivisioError

--- 2x_python_programming_error_handling/0xdivision_calculator.py
+++ 2x_python_programming_error_handling/0xdivision_calculator.py
@@ -1,5 +1,13 @@
-divisor = int (input("input the Divisor: "))
-dividend = int (input("input the Dividend: "))
-Answer = divisor/dividend
-print (f"{divisor} is the divisor and {dividend} is the dividend")
-print (Answer)
+try:
+    
+    divisor = int (input("Enter the Divisor: "))
+    dividend = int (input("Enter the Dividend: "))
+
+    Answer = divisor/dividend
+    
+    print (f"{divisor} is the divisor and {dividend} is the dividend")
+
+    print (Answer)
+
+except ZeroDivisionError:
+    print("Error: Cannot divide by zero!")
