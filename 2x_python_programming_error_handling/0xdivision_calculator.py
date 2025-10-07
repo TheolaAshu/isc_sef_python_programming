b22b83de9d188ca894cf7d9e536673a47b2c235d
codegod <ashutheola@gmail.com>
2025-10-07 18:30:04 +0100

    succesfully resolved conflict

---
+++ 2x_python_programming_error_handling/0xdivision_calculator.py
@@ -0,0 +1,5 @@
+divisor = int (input("input the Divisor: "))
+dividend = int (input("input the Dividend: "))
+Answer = divisor/dividend
+print (f"{divisor} is the divisor and {dividend} is the dividend")
+print (Answer)
